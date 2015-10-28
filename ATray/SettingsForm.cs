73ba844c3dfc6385aa291fec9782e79a21828570
jokedst <jokedst@gmail.com>
2015-10-28 21:46:16 +0100

    Added Ookii dialogs

--- ATray/SettingsForm.cs
+++ ATray/SettingsForm.cs
@@ -44,22 +44,31 @@ namespace ATray
 
         private void addToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            var dialog = new Ookii.Dialogs.VistaFolderBrowserDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                repoList.Items.Add(dialog.SelectedPath).SubItems.AddRange(new[] { "s1", "s2", "s3" });
+            }
+            
+
             //var fbd = new FolderBrowserDialog();
             //var newDir = fbd.ShowDialog();
 
-            // Prepare a dummy string, thos would appear in the dialog
-            string dummyFileName = "Save Here";
+            //// Prepare a dummy string, thos would appear in the dialog
+            //string dummyFileName = "Save Here";
 
-            SaveFileDialog sf = new SaveFileDialog();
-            // Feed the dummy name to the save dialog
-            sf.FileName = dummyFileName;
+            //SaveFileDialog sf = new SaveFileDialog();
+            //// Feed the dummy name to the save dialog
+            //sf.FileName = dummyFileName;
 
-            if (sf.ShowDialog() == DialogResult.OK)
-            {
-                // Now here's our save folder
-                string savePath = Path.GetDirectoryName(sf.FileName);
-                // Do whatever
-            }
+            //if (sf.ShowDialog() == DialogResult.OK)
+            //{
+            //    // Now here's our save folder
+            //    string savePath = Path.GetDirectoryName(sf.FileName);
+            //    // Do whatever
+            //}
         }
     }
 }

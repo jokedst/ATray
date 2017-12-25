1d016b139472ee6c4de360ffd3c867fe3f8283e0
Joakim Wennergren <joakim.wennergren@neovici.se>
2017-12-25 19:10:14 +0100

    Added missing file

---
+++ RepositoryManager.Tests/PostponedEventFacts.cs
@@ -0,0 +1,86 @@
+﻿namespace RepositoryManager.Tests
+{
+    using System.Threading;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PostponedEventFacts
+    {
+        [Test]
+        public void FiresEvent()
+        {
+            var fires = 0;
+            var pe = new PostponedEvent(100, () => fires++);
+
+            pe.StartOrUpdate();
+            Thread.Sleep(200);
+
+            Assert.AreEqual(1,fires);
+        }
+
+        [Test]
+        public void DontFireEventPrematurly()
+        {
+            var fires = 0;
+            var pe = new PostponedEvent(50, () => fires++);
+
+            Thread.Sleep(200);
+            Assert.AreEqual(0, fires);
+
+            pe.StartOrUpdate();
+            Thread.Sleep(200);
+
+            Assert.AreEqual(1, fires);
+        }
+
+        [Test]
+        public void PostponesEvent()
+        {
+            var fires = 0;
+            var pe = new PostponedEvent(100, () => fires++);
+
+            pe.StartOrUpdate();
+            Thread.Sleep(50);
+            pe.StartOrUpdate();
+            Thread.Sleep(50);
+            pe.StartOrUpdate();
+            Thread.Sleep(50);
+            pe.StartOrUpdate();
+            Thread.Sleep(50);
+            pe.StartOrUpdate();
+            Thread.Sleep(150);
+
+            Assert.AreEqual(1, fires);
+        }
+
+        [Test]
+        public void MaxReachedFiresEvent()
+        {
+            var fires = 0;
+            var pe = new PostponedEvent(100000, () => fires++, 3);
+
+            pe.StartOrUpdate();
+            pe.StartOrUpdate();
+            Assert.AreEqual(0, fires);
+            pe.StartOrUpdate();
+
+            Assert.AreEqual(1, fires);
+        }
+
+        [Test]
+        public void CanFireSeveralTimes()
+        {
+            var fires = 0;
+            var pe = new PostponedEvent(50, () => fires++, 3);
+
+            pe.StartOrUpdate();
+            Thread.Sleep(100);
+            pe.StartOrUpdate();
+            Thread.Sleep(100);
+            pe.StartOrUpdate();
+            Thread.Sleep(100);
+
+            Assert.AreEqual(3, fires);
+        }
+    }
+}

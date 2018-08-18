36f5ffa1a762e2dc964fb843d4eb337f3b137ace
Joakim Wennergren <jokedst@gmail.com>
2018-08-18 11:40:14 +0200

    Testing Squirrel with URL paths * upgraded some packages

--- ATray/Properties/AssemblyInfo.cs
+++ ATray/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jokedst")]
 [assembly: AssemblyProduct("ATray")]
-[assembly: AssemblyCopyright("Copyright © Jokedst 2011-2017")]
+[assembly: AssemblyCopyright("Copyright © Jokedst 2011-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]

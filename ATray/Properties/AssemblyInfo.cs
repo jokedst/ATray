c6ffba08b5db8b1b87ec0b8acd7f670b4dc5fc1a
Joakim Wennergren <jokedst@gmail.com>
2018-09-23 13:48:25 +0200

    Official v1.2.6 - bugfix

--- ATray/Properties/AssemblyInfo.cs
+++ ATray/Properties/AssemblyInfo.cs
@@ -3,17 +3,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ATray")]
 [assembly: AssemblyDescription("A tray app that does stuff, e.g. tells you to get up and move arount every 20 minutes")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jokedst")]
 [assembly: AssemblyProduct("ATray")]
 [assembly: AssemblyCopyright("Copyright © Jokedst 2011-2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b4b6516d-9518-4b56-8006-699d681127ec")]
 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.5.0")]
-[assembly: AssemblyFileVersion("1.2.5.0")]
+[assembly: AssemblyVersion("1.2.6.0")]
+[assembly: AssemblyFileVersion("1.2.6.0")]

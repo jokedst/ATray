519b25117969df7726f1fbdde4856589b8d7a50a
Joakim Wennergren <jokedst@gmail.com>
2018-12-27 00:56:07 +0100

    renamed a test class

--- Atray.Tests/Activity/MonthActivitiesFacts.cs
+++ Atray.Tests/Activity/MonthActivitiesFacts.cs
@@ -1,14 +1,14 @@
-﻿namespace Atray.Tests
+﻿namespace Atray.Tests.Activity
 {
     using System;
     using ATray.Activity;
     using NUnit.Framework;
 
     [TestFixture]
-    public class AtrayFacts
+    public class MonthActivitiesFacts
     {
         [Test]
-        public void Something()
+        public void New_MonthActivities_gets_local_computer_name()
         {
             var m = new MonthActivities(2000, 1);
             
